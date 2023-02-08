2a7fda3410aa19809d2a80b8aa90ca2f9e081178
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-02-07 17:31:49 -0700

    📅  | Tue Feb  7 17:31:49 MST 2023

--- src/openAiClient.ts
+++ src/openAiClient.ts
@@ -1,6 +1,12 @@
+import fs from "fs";
+import { PROMPT_FILE } from "./chat-app.js";
 import fetch from "node-fetch";
 export function openAiClient() {
   fetch("https://example.com").then((res) => {
     console.log(res);
   });
+}
+function readPromt() {
+  const prompt = fs.readFileSync(PROMPT_FILE, "utf8");
+  return prompt;
 }
--- src/watchFile.ts
+++ src/watchFile.ts
@@ -1,6 +1,4 @@
-import fs from "fs";
 import choki from "chokidar";
-import { PROMPT_FILE } from "./chat-app.js";
 type ChangeHandler = () => void;
 export function watchFileChange(path: string, changeHandler: ChangeHandler) {
   const watcher = choki.watch(path, {
@@ -8,12 +6,6 @@ export function watchFileChange(path: string, changeHandler: ChangeHandler) {
   });
 
   watcher.on("change", (path) => {
-    console.log(`File ${path} has been changed`);
     changeHandler();
   });
-}
-
-function readPromt() {
-  const prompt = fs.readFileSync(PROMPT_FILE, "utf8");
-  return prompt;
 }
